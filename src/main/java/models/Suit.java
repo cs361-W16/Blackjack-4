c097fd974dbecd54b20c63d7e55e8643f73b221f
zegelink <supercomboooo@gmail.com>
2016-03-09 14:06:34 -0800

    basic model and controller
    initial game

---
+++ src/main/java/models/Suit.java
@@ -0,0 +1,11 @@
+package models;
+
+import java.io.Serializable;
+
+/**
+ * Created by Chongxian Chen on 3/9/16.
+ */
+public enum Suit  implements Serializable {
+    Hearts, Spades, Diamonds, Clubs
+}
+
